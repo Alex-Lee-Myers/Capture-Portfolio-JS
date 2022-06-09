fb5e744adae72b09e098fbb425dcffee4da37ebf
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-06-08 20:00:50 -0400

    information for aboutus section completed. implementing styled-components next and starting basic styling.

---
+++ src/components/FaqSection.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const FaqSection = () => {
+	return (
+		<div className="faq">
+			<h2>
+				Any Questions <span>FAQ</span>
+			</h2>
+			<h4>How Do I Start?</h4>
+			<div className="answer">
+				<p>Lorem ipsum dolor sit amet.</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+					voluptatibus!
+				</p>
+			</div>
+			<h4>Daily Schedule</h4>
+			<div className="answer">
+				<p>Lorem ipsum dolor sit amet.</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+					voluptatibus!
+				</p>
+			</div>
+			<h4>Different Payment Methods</h4>
+			<div className="answer">
+				<p>Lorem ipsum dolor sit amet.</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+					voluptatibus!
+				</p>
+			</div>
+			<h4>What Products Do You Offer?</h4>
+			<div className="answer">
+				<p>Lorem ipsum dolor sit amet.</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+					voluptatibus!
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default FaqSection;
