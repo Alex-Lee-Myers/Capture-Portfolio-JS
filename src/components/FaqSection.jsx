962c3f6926203c3c7a01eeba1ee46a5d2b831c31
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-06-14 21:29:06 -0400

    toggling fun on faq started. didnt have much time to get going though. had to start packing for a move in 2 weeks from now. uggh. hope i can still code every night a little!

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -3,18 +3,26 @@ import styled from "styled-components";
 import { StyledLayout } from "../styles";
 
 const FaqSection = () => {
+	const [faqToggle, setFaqToggle] = React.useState(false);
+
 	return (
 		<StyledFaq>
 			<h2>
 				Any Questions <span>FAQ</span>
 			</h2>
 			<h4>How Do I Start?</h4>
-			<div className="answer">
-				<p>Lorem ipsum dolor sit amet.</p>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
-					voluptatibus!
-				</p>
+			<div onClick={() => setFaqToggle(!faqToggle)} className="answer">
+				{faqToggle ? (
+					<div>
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+							voluptatibus!
+						</p>
+					</div>
+				) : (
+					<div />
+				)}
 			</div>
 			<div className="faq-line"></div>
 			<h4>Daily Schedule</h4>
