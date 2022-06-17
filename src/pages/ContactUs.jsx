646df63747151fed5b6a1e790426293e105bac33
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-06-17 19:34:11 -0400

    small update to contact us page. nothing special. final touches for now coming later tonight or tomorrow.

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { pageAnimation } from "../Animation";
+import { pageAnimation, titleAnim } from "../Animation";
 import { motion } from "framer-motion/dist/framer-motion";
 
 const ContactUs = () => {
@@ -10,16 +10,64 @@ const ContactUs = () => {
 			initial="hidden"
 			animate="show"
 			variants={pageAnimation}
+			style={{ background: "#fff" }}
 		>
-			<h2>Contact Us</h2>
+			<Title>
+				<Hide>
+					<motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+				</Hide>
+			</Title>
+
+			<div>
+				<Social variants={titleAnim}>
+					<Circle />
+					<h2> Send us a Message</h2>
+				</Social>
+
+				<Social variants={titleAnim}>
+					<Circle />
+					<h2> Send us a Message</h2>
+				</Social>
+
+				<Social variants={titleAnim}>
+					<Circle />
+					<h2> Send us a Message</h2>
+				</Social>
+			</div>
 		</ContactUsStyled>
 	);
 };
 
 const ContactUsStyled = styled(motion.div)`
+	padding: 5rem 10rem;
+	color: #353535;
+	min-height: 90vh;
+	@media (max-width: 1500px) {
+		padding: 2rem;
+		font-size: 1rem;
+	}
+`;
+const Title = styled.div`
+	margin-bottom: 4rem;
+	color: black;
+	@media (max-width: 1500px) {
+		margin-top: 5rem;
+	}
+`;
+const Hide = styled.div`
+	overflow: hidden;
+`;
+const Circle = styled.div`
+	border-radius: 50%;
+	width: 3rem;
+	height: 3rem;
+	background: #353535;
+`;
+const Social = styled(motion.div)`
+	display: flex;
+	align-items: center;
 	h2 {
-		padding: 1rem 0rem;
-		color: white;
+		margin: 2rem;
 	}
 `;
 
